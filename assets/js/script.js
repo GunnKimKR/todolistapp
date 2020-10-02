3327c491394b26ba9c4e498965045beb86ec2f83
Kim Geon <62473507+KimGeonkr@users.noreply.github.com>
2020-10-02 09:39:27 +0900

    docs: Add login page input focus style + calendar page markup

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,29 @@
 
 
 const toggleBtnBg = document.querySelector('.btn-toggle-bg');
-const toggleBtn = document.getElementsByClassName('btn-toggle');
-const loginContainer = document.getElementsByClassName('login-container');
+const toggleBtnBox = document.querySelector('.sign-form-btn-toggle-box');
+const sginForm = document.querySelectorAll('.sign-form');
 
-toggleBtn[1].addEventListener('click', () => {
-    toggleBtnBg.style.left = '50%';
-    toggleBtn[0].classList.remove('active');
-    toggleBtn[1].classList.add('active');
-    for(var slide of loginContainer) {
-        slide.classList.add('active');
-    }
-});
+// console.log(toggleBtnBox);
+// toggleBtnBox.addEventListener('click', (evt) => {
+//     console.log(evt.target.parentNode);
+// });
 
-toggleBtn[0].addEventListener('click', () => {
-    toggleBtnBg.style.left = '0';
-    toggleBtn[1].classList.remove('active');
-    toggleBtn[0].classList.add('active');
-    for(var slide of loginContainer) {
-        slide.classList.remove('active');
-    }
-});
+// toggleBtn[1].addEventListener('click', () => {
+//     toggleBtnBg.style.left = '50%';
+//     toggleBtn[0].classList.remove('active');
+//     toggleBtn[1].classList.add('active');
+//     for(var slide of sginForm) {
+//         slide.classList.add('active');
+//     }
+// });
+
+// toggleBtn[0].addEventListener('click', () => {
+//     toggleBtnBg.style.left = '0';
+//     toggleBtn[1].classList.remove('active');
+//     toggleBtn[0].classList.add('active');
+//     for(var slide of sginForm) {
+//         slide.classList.remove('active');
+//     }
+// });
+
